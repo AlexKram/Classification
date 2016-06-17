00853535553e69d0a6bbf7ccfbe278bfa963dca3
AlexKram <alexander.kramlich@student.kit.edu>
2016-06-18 01:41:02 +0200

    Slight changes to the visualisation script.

--- display_data.m
+++ display_data.m
@@ -1,27 +1,39 @@
 % DISPLAY_DATA.M
 %
 % ------------------------------------------------
-% Version 1.0.0.
+% Version 1.1.0.
 % Created       - 13.06.2016 Alexander Kramlich
-% Last modified - 13.06.2016 Alexander Kramlich
+% Last modified - 17.06.2016 Alexander Kramlich
 % ------------------------------------------------
 
-subjectNumber = 1;
-dataToPlot = totalaccxtrain;
+close all
 
-[R,C] = size(bodyaccxtrain);
-T = sum(subjecttrain == subjectNumber);
-t = linspace(0, T*C/50, T*C);
+% User input
+subjectNumber = 1;
+
+Fs = 50;                                    % Sampling frequency
+
+relevantData = totalaccxtrain(subjecttrain == subjectNumber, 1:64);
+relevantLabels = labeltrain(subjecttrain == subjectNumber);
+[R,~] = size(relevantData);
 
 color_order = get(groot, 'defaultAxesColorOrder');
-figure
+figure('Position', [720 300 1200 700])
 hold on
 
+p = zeros(1,6);                             % Chart line object which will be used later in the legend
 for r=1:R
-    plot(1+(r-1)*64:r*64, totalaccxtrain(r,1:64), 'Color', color_order(labeltrain(r),:))
-    if subjecttrain(r) ~= 1
-        break;
-    end
+    t = (1+(r-1)*64:r*64)/Fs;               % Division by sampling frequency Fs in order to convert samples to time
+    
+    % Plot one row with the color belonging to the corresponding label
+    p(relevantLabels(r)) = plot(t, relevantData(r,:), 'Color', color_order(relevantLabels(r),:));
 end
-title('Subject #1')
+
+xlim([0 t(end)])
+title(sprintf('Subject #%g', subjectNumber))
+xlabel('Time [s]')
+ylabel('totalaccxtrain')
+legend(p, activitylabels)
 hold off
+
+clear Fs p r R subjectNumber t T
